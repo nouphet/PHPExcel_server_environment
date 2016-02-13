e0002e36d519567324f1c8adb7b3fb2029e72246
nouphet <nouphet.ratanavong@gmail.com>
2016-02-14 04:24:42 +0900

    Change path to Classes

--- get_server_environment.php
+++ get_server_environment.php
@@ -1,6 +1,6 @@
 <?php
 
-set_include_path(get_include_path() . PATH_SEPARATOR . 'Classes/');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'PHPExcel/Classes/');
 include 'PHPExcel.php';
 include 'PHPExcel/IOFactory.php';
 
